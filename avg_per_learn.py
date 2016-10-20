81a6b2ed992e72ba8a7540b3f76b54214789c36a
Ajay Malalikar <ajay.malalikar@gmail.com>
2016-10-19 18:03:40 -0700

    Optimized per_learn.py using profilers

--- avg_per_learn.py
+++ avg_per_learn.py
@@ -3,19 +3,22 @@ import sys
 import random
 import timeit
 import json
-max_iter = 20
 
 
 def main(path):
     file_list = []
     b = 0
     vocab = {}
+    weighted_vocab = {}
+    beta = 0
+    c = 1
+
     for root, subdir, files in os.walk(path):
         if len(files):
             for f in files:
-                file_list.append(root + "/" + f)
+                file_list.append(os.path.join(root, f))
 
-    for i in range(20):
+    for i in range(30):
         random.shuffle(file_list)
         n = len(file_list)
         for j in range(n):
@@ -27,21 +30,28 @@ def main(path):
                 content = fs.read().strip().split()
                 for word in content:
                     if word in vocab:
-                        alpha += vocab[word]
+                        alpha += vocab[word][0]
 
-                if y*alpha <= 0:
+                if y * alpha <= 0:
                     for word in content:
                         if word in vocab:
-                            vocab[word] += y
+                            vocab[word][0] += y
+                            vocab[word][1] += y*c
                         else:
-                            vocab[word] = y
+                            vocab[word] = [y, y*c]
                     b += y
-    model = (vocab, b)
+                    beta += y * c
+    for k, v in vocab.items():
+        weighted_vocab[k] = vocab[k][0] - ((1 / c) * vocab[k][1])
+    beta = 1 - ((1 / c) * beta)
+
+    model = (weighted_vocab, beta)
     with open("per_model.txt", 'w') as fs:
         fs.write(json.dumps(model))
+
 
 if __name__ == "__main__":
     start = timeit.default_timer()
     main(sys.argv[1])
     end = timeit.default_timer()
-    print("Update process:" + str(end - start))
+    print("Avg process:" + str(end - start))
--- per_learn.py
+++ per_learn.py
@@ -4,44 +4,64 @@ import random
 import timeit
 import json
 max_iter = 20
+import cProfile
 
-
-def main(path):
+def main():
+    path = "train"
     file_list = []
     b = 0
     vocab = {}
+    file_contents = {}
+
     for root, subdir, files in os.walk(path):
         if len(files):
             for f in files:
-                file_list.append(root + "/" + f)
+                file_list.append(os.path.join(root, f))
 
-    for i in range(20):
-        random.shuffle(file_list)
+    for i in range(max_iter):
+        # No shuffle required for first iteration
+        if i != 0:
+            random.shuffle(file_list)
+
         n = len(file_list)
         for j in range(n):
             file_path = file_list[j]
             alpha = b
             y = 1 if "spam" in file_path else -1
 
-            with open(file_path, "r", encoding="latin1") as fs:
-                content = fs.read().strip().split()
+            # Avoid opening and reading the file for each iteration. So save it in dictionary
+            if i == 0:
+                with open(file_path, "r", encoding="latin1") as fs:
+                    if i == 0:
+                        content = fs.read().strip().split()
+                        file_contents[file_path] = content
+            else:
+                content = file_contents[file_path]
+            if j == 0:
+                alpha = 0
+            else:
+                if word in vocab:
+                    alpha += vocab[word]
+            if y*alpha <= 0:
                 for word in content:
                     if word in vocab:
-                        alpha += vocab[word]
+                        vocab[word] += y
+                    else:
+                        vocab[word] = y
+                b += y
 
-                if y*alpha <= 0:
-                    for word in content:
-                        if word in vocab:
-                            vocab[word] += y
-                        else:
-                            vocab[word] = y
-                    b += y
-    model = (vocab, b)
     with open("per_model.txt", 'w') as fs:
-        fs.write(json.dumps(model))
+        fs.write(json.dumps((vocab, b)))
+
 
 if __name__ == "__main__":
     start = timeit.default_timer()
-    main(sys.argv[1])
+    main()
     end = timeit.default_timer()
     print("Update process:" + str(end - start))
+    cProfile.run('main()', 'myFunction.profile')
+
+
+import pstats
+stats = pstats.Stats('myFunction.profile')
+stats.strip_dirs().sort_stats('time').print_stats()
